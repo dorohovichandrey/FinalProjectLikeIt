77fd80afa66b39f90b507080055254b1b67d2fda
dorohovichandrey <dorohovichandrey@gmail.com>
2017-01-26 17:00:39 +0300

    refactored ChangeLanguageCommand

--- src/main/java/by/dorohovich/site/command/impl/ChangeLanguageCommand.java
+++ src/main/java/by/dorohovich/site/command/impl/ChangeLanguageCommand.java
@@ -10,15 +10,16 @@ import javax.servlet.http.HttpSession;
  * Created by User on 01.12.2016.
  */
 public class ChangeLanguageCommand implements Command {
-    private static final String PARAM_LANG = "lang";
-    private static final String ATR_LOCALE = "locale";
-    private static final String ATR_PAGE = "page";
+    private static final String LOCALE_PARAM = "lang";
+    private static final String LOCALE_ATTR = "locale";
+    private static final String KEY_FOR_PAGE_ATTR = "page";
 
     public String execute(HttpServletRequest request) {
-        String lang = request.getParameter(PARAM_LANG);
+        String locale = request.getParameter(LOCALE_PARAM);
         HttpSession session = request.getSession(true);
-        session.setAttribute(ATR_LOCALE, lang);
-        String currPage = (String) session.getAttribute(ATR_PAGE);
-        return MappingManager.getProperty(currPage);
+        session.setAttribute(LOCALE_ATTR, locale);
+        String keyForCurPage = (String) session.getAttribute(KEY_FOR_PAGE_ATTR);
+        String curPage = MappingManager.getProperty(keyForCurPage);
+        return curPage;
     }
 }
