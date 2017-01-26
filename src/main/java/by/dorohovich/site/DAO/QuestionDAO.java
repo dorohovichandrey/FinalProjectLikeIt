06f99c6664857804b3ee9d2c44ac347507762e09
dorohovichandrey <dorohovichandrey@gmail.com>
2017-01-26 22:58:28 +0300

    added Question

---
+++ src/main/java/by/dorohovich/site/DAO/QuestionDAO.java
@@ -0,0 +1,7 @@
+package by.dorohovich.site.DAO;
+
+/**
+ * Created by User on 26.01.2017.
+ */
+public class QuestionDAO {
+}
--- src/main/java/by/dorohovich/site/entity/Question.java
+++ src/main/java/by/dorohovich/site/entity/Question.java
@@ -1,7 +1,84 @@
 package by.dorohovich.site.entity;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 /**
  * Created by User on 26.01.2017.
  */
-public class Question {
+public class Question extends Entity<Integer> {
+
+    private User owner;
+    private Timestamp dateAndTime;
+    private String text;
+    private Theme theme;
+    private Integer rating;
+    private String header;
+
+    public Question(User owner, String text, Theme theme, String header) {
+        this.owner = owner;
+        this.text = text;
+        this.theme = theme;
+        this.header = header;
+        dateAndTime = Timestamp.valueOf(LocalDateTime.now());
+        rating = 0;
+    }
+
+    public Question(Integer id, User owner, Timestamp dateAndTime, String text, Theme theme, Integer rating, String header) {
+        super(id);
+        this.owner = owner;
+        this.dateAndTime = dateAndTime;
+        this.text = text;
+        this.theme = theme;
+        this.rating = rating;
+        this.header = header;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Timestamp getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(Timestamp dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+
+    public void setTheme(Theme theme) {
+        this.theme = theme;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
 }
