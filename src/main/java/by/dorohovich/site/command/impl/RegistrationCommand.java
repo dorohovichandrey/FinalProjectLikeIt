2e89802bf387ad432b0125b1baae992d093815d6
dorohovichandrey <dorohovichandrey@gmail.com>
2017-01-26 17:05:43 +0300

    refactored RegistrationCommand

--- src/main/java/by/dorohovich/site/command/impl/RegistrationCommand.java
+++ src/main/java/by/dorohovich/site/command/impl/RegistrationCommand.java
@@ -11,10 +11,11 @@ import by.dorohovich.site.service.UserService;
 import by.dorohovich.site.utility.MappingManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 /**
  * Created by User on 08.12.2016.
  */
-public class RegistrationCommand  implements Command {
+public class RegistrationCommand implements Command {
     private static final Logger LOGGER = LogManager.getLogger();
 
     private static final String LOGIN_PARAM = "login";
@@ -25,20 +26,18 @@ public class RegistrationCommand  implements Command {
     private static final String LOGIN_ATTR = "login";
     private static final String PASSWORD_ATTR = "password";
     private static final String PASSWORD_CONFIRMATION_ATTR = "passwordConfirmation";
-    private static final String EMAIL_ADDR_ATR = "emailAddr";
-    private static final String IS_LOGIN_FREE_ATR = "isLoginFree";
+    private static final String EMAIL_ADDR_ATTR = "emailAddr";
+    private static final String IS_LOGIN_FREE_ATTR = "isLoginFree";
 
     private static final String KEY_FOR_PAGE_IF_SUCCESS = "page.logIn";
     private static final String KEY_FOR_PAGE_IF_FAILED = "page.registration";
-
 
 
     @Override
     public String execute(HttpServletRequest request) throws CommandException {
         try {
             return tryExecute(request);
-        }
-        catch (ServiceException e) {
+        } catch (ServiceException e) {
             LOGGER.error("Problem with userService, while trying to register", e);
             throw new CommandException(e);
         }
@@ -50,7 +49,7 @@ public class RegistrationCommand  implements Command {
         String email = request.getParameter(EMAIL_ADDR_PARAM);
         UserService userService = new UserService();
         boolean isLoginFree = userService.checkIsLoginFree(login);
-        if(isLoginFree) {
+        if (isLoginFree) {
             userService.register(login, password, email);
         }
         packAttributes(login, password, email, isLoginFree, request);
@@ -60,15 +59,15 @@ public class RegistrationCommand  implements Command {
 
     private void packAttributes(String login, String password, String email, boolean isLoginFree, HttpServletRequest request) {
         HttpSession session = request.getSession(true);
-        session.setAttribute(IS_LOGIN_FREE_ATR, isLoginFree);
-        if( !isLoginFree){
+        session.setAttribute(IS_LOGIN_FREE_ATTR, isLoginFree);
+        if (!isLoginFree) {
             session.setAttribute(LOGIN_ATTR, login);
             session.setAttribute(PASSWORD_ATTR, password);
-            session.setAttribute(EMAIL_ADDR_ATR, email);
+            session.setAttribute(EMAIL_ADDR_ATTR, email);
         }
     }
 
-    private String choosePage(boolean isLoginFree){
+    private String choosePage(boolean isLoginFree) {
         String key = isLoginFree ? KEY_FOR_PAGE_IF_SUCCESS : KEY_FOR_PAGE_IF_FAILED;
         String page = MappingManager.getProperty(key);
         return page;
